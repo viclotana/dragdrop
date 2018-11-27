f63f081375e7b90124f2ddb64574a341b8b66cc0
viclotana <viclotana@gmail.com>
2018-11-27 14:44:37 +0100

    commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'dragdrop';
+  artists = [
+    'Artist I - Davido',
+    'Artist II - Wizkid',
+    'Artist III - Burna Boy',
+    'Artist IV - Kiss Daniel',
+    'Artist V - Mayorkun',
+    'Artist VI - Mr. Eazi',
+    'Artist VII - Tiwa Savage',
+    'Artist VIII - Blaqbonez',
+    'Artist IX - Banky W',
+    'Artist X - Yemi Alade',
+    'Artist XI - Perruzi',
+    'Artist XII - Seyi Shay',
+    'Artist XIII - Teni'
+  ];
+
+  alteArtists = [
+    'Artist 1 - Odunsi',
+    'Artist 2 - Nonso',
+    'Artist 3 - Wavy the creator',
+    'Artist 4 - Dwin',
+    'Artist 5 - SDC',
+    'Artist 6 - Teni'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer !== event.container) {
+      transferArrayItem(event.previousContainer.data,event.container.data, 
+        event.previousIndex, event.currentIndex)
+    } else {
+      moveItemInArray(this.artists, event.previousIndex, event.currentIndex);
+    }
+  }
 }
